68384b31751cf3b7052e2f371f1212b4c1de4ac6
SonnenPingu <126945262+SonnenPingu@users.noreply.github.com>
2023-10-03 01:33:15 +0200

    Update extractModeratorImageUrl.js

--- Function/Sync/extractModeratorImageUrl.js
+++ Function/Sync/extractModeratorImageUrl.js
@@ -1,4 +1,4 @@
-// Funktion zum Extrahieren der Bild-URL f�r den Moderator
+// Funktion zum Extrahieren der Bild-URL für den Moderator
 function extractModeratorImageUrl(htmlData, moderatorName) {
     const $ = cheerio.load(htmlData);
     const imgTags = $('img.img-responsive');
@@ -6,12 +6,18 @@ function extractModeratorImageUrl(htmlData, moderatorName) {
     for (let i = 0; i < imgTags.length; i++) {
         const imgTag = $(imgTags[i]);
         const src = imgTag.attr('src');
-        if (src && src.includes(moderatorName)) {
+        const normalizedModeratorName = moderatorName
+            .replace(/[ö]/g, 'oe')
+            .replace(/[ü]/g, 'ue')
+            .replace(/[ä]/g, 'ae')
+            .toLowerCase();
+
+        if (src && src.toLowerCase().includes(normalizedModeratorName)) {
             return src;
         }
     }
 
-    // Wenn kein Bild gefunden wurde, gib einen Standard-URL zur�ck
+    // Wenn kein Bild gefunden wurde, gib einen Standard-URL zurück
     return 'https://mystic-celduin.de/wp-content/uploads/2023/10/WhatsApp-Bild-2023-08-31-um-21.10.02.jpg';
 }
 module.exports = extractModeratorImageUrl
